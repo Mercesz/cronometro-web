3f276baf298cb7bd92847c27933ead329f126bbf
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-11-06 20:36:20 -0300

    feat: Alterando estilo do cronometro, inserindo resposividade e lógica para funcionamento

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+const watchDocument = document.querySelector("#watch")
+let seconds = 0
+let minutes = 0
+let hours = 0
+let interval
+let isRunnig = false
+
+
+function init() {
+    if (isRunnig) return
+
+    isRunnig = true
+    watch()
+    interval = setInterval(watch, 1000)
+}
+
+const pause = () => {
+    clearInterval(interval)
+    isRunnig = false
+}
+
+const clearAll = () => {
+    clearInterval(interval)
+    seconds = 0
+    minutes = 0
+    hours = 0
+    watchDocument.innerHTML = "00:00:00"
+    isRunnig = false
+}
+
+const digitZero = (digit) => {
+    if (digit < 10) {
+        return `0${digit}`
+    } else {
+        return digit
+    }
+}
+
+
+function watch() {
+    seconds++
+
+    if (seconds === 60) {
+        minutes++
+        seconds = 0
+    }
+    if (minutes === 60) {
+        minutes = 0
+        hours++
+    }
+
+    watchDocument.innerHTML =
+        digitZero(hours) + ":" + digitZero(minutes) + ":" + digitZero(seconds)
+}
